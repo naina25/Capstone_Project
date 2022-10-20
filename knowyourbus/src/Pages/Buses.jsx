406b51963ea733e8a4b555cf34c022563f502e01
Deveshkr-19 <Devesh.Singh@bhavnacorp.com>
2022-10-20 16:57:41 +0530

    Bus Card section create

--- knowyourbus/src/Pages/Buses.jsx
+++ knowyourbus/src/Pages/Buses.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import BusCards from "../Components/BusesComponent/BusCards/BusCards";
 
 const Buses = () => {
-    return <section className="busees-section">Buses</section>;
+    return <section className="busees-section">
+        <BusCards />
+    </section>;
 };
 
 export default Buses;
