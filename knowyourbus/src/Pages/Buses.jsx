ef14baa4e3ae42386303a138bb90240804ab22a6
Deveshkr-19 <Devesh.Singh@bhavnacorp.com>
2022-10-20 17:31:28 +0530

    Add bus card to bus page component

--- knowyourbus/src/Pages/Buses.jsx
+++ knowyourbus/src/Pages/Buses.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import BusCards from "../Components/BusesComponent/BusCards/BusCards";
-import Amenities from "../Components/BusesComponent/Amenities/Amenities";
-import Policies from "../Components/BusesComponent/Policies/Policies";
+// import BusCards from "../Components/BusesComponent/BusCards/BusCards";
+// import Amenities from "../Components/BusesComponent/Amenities/Amenities";
+// import Policies from "../Components/BusesComponent/Policies/Policies";
+import SearchForm from "../Components/SearchForm/SearchForm";
+import BusPage from "../Components/BusesComponent/BusPage/BusPage";
 
 const Buses = () => {
 	return (
-		<section className="busees-section">
-			<BusCards />
-			<Amenities />
-			<Policies />
+		<section className="buses-section">
+			<SearchForm />
+			<BusPage />
 		</section>
 	);
 };
