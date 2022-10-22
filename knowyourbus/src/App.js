749bab967a8b8cc2a784aa4bcae8a10eaddcd9de
Deveshkr-19 <Devesh.Singh@bhavnacorp.com>
2022-10-22 17:57:00 +0530

    Buses cmoponent UI complete and GET request integration

--- knowyourbus/src/App.js
+++ knowyourbus/src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Navbar from "./Components/Shared/Navbar/Navbar";
@@ -8,19 +7,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Buses from "./Pages/Buses";
 
 function App() {
-    return (
-        <div className="App">
-            <Navbar />
-            <Router>
-                <Routes>
-                    <Route path="/" exact element={<Home />}></Route>
-                    <Route path="/login" element={<Login />}></Route>
-                    <Route path="/buses" element={<Buses />}></Route>
-                </Routes>
-            </Router>
-            <Footer />
-        </div>
-    );
+	return (
+		<div className="App">
+			<Navbar />
+			<Router>
+				<Routes>
+					<Route path="/" exact element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/buses" element={<Buses />} />
+				</Routes>
+			</Router>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
