d7403965dfc04a96cb58ee2dcd85a27e7fa72be7
MuditArya3 <Mudit.Arya@bhavnacorp.com>
2022-10-28 10:01:36 +0530

    profileCodeUpdated

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,10 +6,10 @@ import Footer from "./Components/Shared/Footer/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Buses from "./Pages/Buses";
 import Profile from "./Pages/Profile";
-import BookNow from "./Components/BusesComponent/BookNow/BookNow";
 import Foffers from "./Pages/Foffers";
 import ContactUs from "./Pages/ContactUs";
 import Tripy from "./Pages/Tripy";
+import BookNow from "./Components/BusesComponent/BookNow/BookNow";
 
 
 
