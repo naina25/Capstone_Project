73f21b0b9c98ff089c3c1e28e7c863feda4e432b
Deveshkr-19 <Devesh.Singh@bhavnacorp.com>
2022-10-30 23:49:41 +0530

    Logout complete and review styles

--- Frontend/src/Components/LoginComponent/LoginForm/LoginForm.jsx
+++ Frontend/src/Components/LoginComponent/LoginForm/LoginForm.jsx
@@ -5,109 +5,64 @@ import Cookies from "universal-cookie";
 import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
-    const [success, setSuccess] = useState(false);
-    const [password, setPassword] = useState("");
-    const [username, setUsername] = useState("");
-    const [JwtToken, setJwtToken] = useState();
+	const [password, setPassword] = useState("");
+	const [username, setUsername] = useState("");
 
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-        const data = {
-            Username: username,
-            Password: password,
-        };
+		const data = {
+			Username: username,
+			Password: password,
+		};
 
-        await axios
-            .post("https://localhost:44387/api/login", data)
-            .then((res) => {
-                alert("login done");
-                setSuccess(true);
+		await axios
+			.post("https://localhost:44387/api/login", data)
+			.then((res) => {
+				const token = res.data;
+				const cookies = new Cookies();
+				cookies.set("my_cookie", token);
+				navigate("/");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 
-                const token = res.data;
+		window.location.reload();
+	};
 
-                const cookies = new Cookies();
-                cookies.set("my_cookie", token);
-                navigate("/");
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-
-        window.location.reload();
-    };
-
-    return (
-        <>
-            {success ? (
-                <section>
-                    <div class="main-content">
-                        <div className="card">
-                            <img
-                                className="profile_image"
-                                src="https://theutkarshgupta.com/wp-content/uploads/2021/02/webmaster2-about-pic1.png"
-                                alt="John"
-                            />
-
-                            <h1>{JwtToken.User_Name}</h1>
-                            <p className="title">
-                                Contact No. {JwtToken.Phone}
-                            </p>
-                            <p>Harvard University</p>
-                            <div>
-                                <a href="/#">
-                                    <i className="fa fa-dribbble"></i>
-                                </a>
-                                <a href="/#">
-                                    <i className="fa fa-twitter"></i>
-                                </a>
-                                <a href="/#">
-                                    <i className="fa fa-linkedin"></i>
-                                </a>
-                                <a href="/#">
-                                    <i className="fa fa-facebook"></i>
-                                </a>
-                            </div>
-                            <p>
-                                <button>Contact</button>
-                            </p>
-                        </div>
-                    </div>
-                </section>
-            ) : (
-                <div className="login-form">
-                    <div className="heading-banner">
-                        <h2>Welcome Back!</h2>
-                    </div>
-                    <form onSubmit={handleSubmit} className="form-body">
-                        <div className="login-inputs">
-                            <label htmlFor="username">Username</label>
-                            <input
-                                id="username"
-                                type="text"
-                                placeholder="Enter Username"
-                                onChange={(e) => setUsername(e.target.value)}
-                                autoComplete="on"
-                            ></input>
-                        </div>
-                        <div className="login-inputs">
-                            <label htmlFor="password">Password</label>
-                            <input
-                                id="password"
-                                type="password"
-                                placeholder="Enter Password"
-                                onChange={(e) => setPassword(e.target.value)}
-                                autoComplete="off"
-                            ></input>
-                        </div>
-                        <button type="submit">Login</button>
-                    </form>
-                </div>
-            )}
-        </>
-    );
+	return (
+		<div className="login-form">
+			<div className="heading-banner">
+				<h2>Welcome Back!</h2>
+			</div>
+			<form onSubmit={handleSubmit} className="form-body">
+				<div className="login-inputs">
+					<label htmlFor="username">Username</label>
+					<input
+						id="username"
+						type="text"
+						placeholder="Enter Username"
+						onChange={(e) => setUsername(e.target.value)}
+						autoComplete="on"
+					></input>
+				</div>
+				<div className="login-inputs">
+					<label htmlFor="password">Password</label>
+					<input
+						id="password"
+						type="password"
+						placeholder="Enter Password"
+						onChange={(e) => setPassword(e.target.value)}
+						autoComplete="off"
+					></input>
+				</div>
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	);
 };
 
 export default LoginForm;
--- Frontend/src/Context/auth.context.js
+++ Frontend/src/Context/auth.context.js
@@ -1,38 +1,46 @@
 import React, { createContext, useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
 import axios from "axios";
+import Cookies from "universal-cookie";
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = (props) => {
-    const [userToken, setUserToken] = useState(props.tokenData);
-    const [authSuccess, setAuthSuccess] = useState();
+	const [userToken, setUserToken] = useState(props.tokenData);
+	const [authSuccess, setAuthSuccess] = useState();
 
-    useEffect(() => {
-        const authUser = async () => {
-            try {
-                const decoded = await jwt_decode(userToken);
-                await axios
-                    .get(
-                        `https://localhost:44387/api/Auth/${decoded.UserAadhar}`
-                    )
-                    .then((res) => {
-                        if (res.status === 200) {
-                            setAuthSuccess(true);
-                        } else setAuthSuccess(false);
-                    });
-            } catch (error) {
-                setAuthSuccess(false);
-            }
-        };
-        authUser();
-    }, [userToken]);
+	useEffect(() => {
+		const authUser = async () => {
+			try {
+				const decoded = await jwt_decode(userToken);
+				await axios
+					.get(
+						`https://localhost:44387/api/Auth/${decoded.UserAadhar}`
+					)
+					.then((res) => {
+						if (res.status === 200) {
+							setAuthSuccess(true);
+						} else setAuthSuccess(false);
+					});
+			} catch (error) {
+				setAuthSuccess(false);
+			}
+		};
+		authUser();
+	}, [userToken]);
 
-    return (
-        <AuthContext.Provider value={{ userToken, setUserToken, authSuccess }}>
-            {props.children}
-        </AuthContext.Provider>
-    );
+	const logout = () => {
+		const cookies = new Cookies();
+		cookies.remove("my_cookie");
+	};
+
+	return (
+		<AuthContext.Provider
+			value={{ userToken, setUserToken, authSuccess, logout }}
+		>
+			{props.children}
+		</AuthContext.Provider>
+	);
 };
 
 export const useAuth = () => React.useContext(AuthContext);
